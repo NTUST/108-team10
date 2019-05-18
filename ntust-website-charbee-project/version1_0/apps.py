09a6af7416b5bedb08aaa7612326be4f80a0d818
KuoHsu <33677654+KuoHsu@users.noreply.github.com>
2019-05-18 13:57:33 +0800

    'delete'

--- ntust-website-charbee-project/version1_0/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Version10Config(AppConfig):
-    name = 'version1_0'
--- ntust-website-charbee-project/version1_0/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import *
-import datetime as d
-
-def hello(request):
-    dd = d.datetime.now()
-    #return HttpResponse('<h1>Hello World!</h1>')
-    return render(request,'test_index.html',{'time':dd})
-# Create your views here.
